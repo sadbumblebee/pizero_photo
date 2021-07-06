ae69eb676692375afc02946388839d9eb28c6dc2
Daniel Wolfe <the.daniel.wolfe@gmail.com>
2021-07-05 17:21:15 -0700

    update: potential async fix

--- camera.py
+++ camera.py
@@ -25,7 +25,7 @@ dev = InputDevice('/dev/input/event0')
 async def helper(dev):
     async for ev in dev.async_read_loop():
         print(repr(ev))
-        photo_button_is_pressed = True
+        return True
 
 try:
     from PIL import Image
@@ -281,7 +281,10 @@ def main():
         #     print(categorize(event))
         #     photo_button_is_pressed = True
         loop = asyncio.get_event_loop()
-        loop.run_until_complete(helper(dev))
+        done, _ = loop.run_until_complete(asyncio.wait(helper(dev)))
+        for fut in done:
+            print('done')
+        loop.close()
 
 #        if GPIO.event_detected(CAMERA_BUTTON_PIN):
 #            sleep(DEBOUNCE_TIME)
