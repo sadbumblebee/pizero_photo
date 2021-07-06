fe6fea1e2a53c9b25e08c287f523e573806aeca8
Daniel Wolfe <the.daniel.wolfe@gmail.com>
2021-07-05 17:42:35 -0700

    update: attempt to add debounce to touch

--- camera.py
+++ camera.py
@@ -270,13 +270,15 @@ def main():
     GPIO.add_event_detect(CAMERA_BUTTON_PIN, GPIO.FALLING)
     GPIO.add_event_detect(EXIT_BUTTON_PIN, GPIO.FALLING)
     while True:
-        photo_button_is_pressed = None
-        exit_button_is_pressed = None
         for event in dev.read_loop():
+            photo_button_is_pressed = None
+            exit_button_is_pressed = None
 
             if event.type == ecodes.EV_KEY:
-                print(categorize(event))
-                photo_button_is_pressed = True
+                sleep(DEBOUNCE_TIME)
+                if event.type == ecodes.EV_KEY:
+                    print(categorize(event))
+                    photo_button_is_pressed = True
 
     #        if GPIO.event_detected(CAMERA_BUTTON_PIN):
     #            sleep(DEBOUNCE_TIME)
